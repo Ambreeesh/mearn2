9a84c41e9d6ecce4de9a0311994972c12ff04a1d
Ambreesh Upadhyay <onambreeshs4@gmail.com>
2026-01-12 00:30:19 +0530

    HTTP Server

--- Session1/index.js
+++ Session1/index.js
@@ -1,7 +1,22 @@
 const http = require("http");
-const PORT = 8082
-const server = http.createServer(() => {
-   console.log(`Request incomming...`);
+const PORT = 8081
+const server = http.createServer((req, res) => {
+            const serverInfo = {
+        serverName: "Crio Server",
+        version: "1.0.0",
+        currentDate: new Date().toLocaleDateString(),
+        currentTime: new Date().toLocaleTimeString(),
+        };
+   if (req.url === "/status") {
+    res.writeHead(200, { "Content-Type": "application/json" }); //Adding Headers
+    res.write(JSON.stringify(serverInfo));
+    res.end();
+  } else {
+    res.writeHead(200, { "Content-Type": "text/html" }); //Adding Headers
+    res.write(`<h1>Hello from server</h1>`);
+    res.end();
+  }
+;
 });
 
 server.listen(PORT, ()=>{
