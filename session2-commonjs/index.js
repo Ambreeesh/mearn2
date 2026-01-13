139f675dd7dd801d5cc184b52226ed8a565b38c6
Ambreesh Upadhyay <onambreeshs4@gmail.com>
2026-01-13 23:27:19 +0530

    commonjs

---
+++ session2-commonjs/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// const currenciesJson = require("./currencies.json");
+const axios = require("axios");
+const {getCurrencies, getCurrenciesBySymbol} = require ("./controllers/currencies.controllers.js");
+const app = express();
+const PORT = 8082;
+
+app.get("/", (req, res)=>{
+  res.send("<h1>Currency Database</h1>");
+});
+
+app.get("/currencies", getCurrencies)
+
+app.get("/currencies/:symbol", getCurrenciesBySymbol);
+
+app.listen(PORT,()=>{
+  console.log(`Server is runing on ${PORT}`);
+})
